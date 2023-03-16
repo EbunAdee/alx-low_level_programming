d19c1bcb442316e4b33eb994e1b3c961ff6cec9c
Esther <estheridonije@gmail.com>
2023-03-16 14:03:57 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-*Return Always 0 (Success)
-*/
+ * main - 5-printf.c
+ * description - Write a C program that prints exactly with proper grammar,
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  print("with proper grammar, but the outcome is a piece of art,\n");
+  printf("with proper grammar, but the outcome is a piece of art,\n");
   return (0);
 }
