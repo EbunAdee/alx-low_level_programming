8c44d57801803da4099c99cddad72008e51ff7aa
Esther <estheridonije@gmail.com>
2023-03-22 06:44:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,11 +22,13 @@ _putchar(' ');
 _putchar(k + '0');
 }
 else
-(k >= 1);
+if (k >= 1);
+{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
+}
 }
 }
 _putchar('\n');
