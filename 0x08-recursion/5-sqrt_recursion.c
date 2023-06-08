febb0fe7021422cecd4357a4103c92f0af370118
EbunAdee <estheridonije@gmail.com>
2023-06-07 18:37:08 -0700

    task 5 to 8

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * power_root - returns the natural square root of a number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
+ */
+int power_root(int i, int j)
+{
+	if (j % (i / j) == 0)
+	{
+		if (j * (i / j) == i)
+			return (j);
+		else
+			return (-1);
+	}
+	return (0 + power_root(i, j + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_root(n, 2));
+}
