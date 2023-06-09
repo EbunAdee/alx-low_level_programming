5c168cc4c04fafe211f1982b946ec5c8eec53267
EbunAdee <estheridonije@gmail.com>
2023-06-09 15:56:32 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,9 +18,12 @@ int main(int argc, char *argv[])
 		printf("error\n");
 		return (1);
 	}
+		
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		mult = num1 * num2;
+
 		printf("%d\n", mult);
+		
 		return (0);
 }
