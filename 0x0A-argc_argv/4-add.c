861462db5cf819dd9ff6e7d9e42cb94902537e30
EbunAdee <estheridonije@gmail.com>
2023-06-09 16:10:46 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,14 +6,14 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the argument.
  * Return: Returns 1,If one of the number contains symbols that are not digits
-           Returns 0, if none of the number contains symbols that are not digits.
+ *         Returns 0, if none of the number contains symbols that are not digits.
  */
 
 int main(int argc, char *argv[])
 {
 	int no, digit, add;
 
-	for (no = 1; no < argc; no++)
+for (no = 1; no < argc; no++)
 	{
 		for (digit = 0; argv[no][digit]; digit++)
 		{
