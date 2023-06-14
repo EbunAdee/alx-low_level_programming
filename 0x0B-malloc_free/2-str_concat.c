f028034224a43cc6d8f3b78d4f09b91bd0f6aee8
EbunAdee <estheridonije@gmail.com>
2023-06-14 06:12:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,30 +18,21 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[j] != '\0')
 	{
 		i++;
-	}
-
-	while (s2[j] != '\0')
-	{
 		j++;
 	}
-
 	concat = malloc((i + j + 1) * sizeof(char));
-
 	if (concat == NULL)
 	{
 		free(concat);
 		return (NULL);
 	}
-
 	for (m = 0; s1[m] != '\0'; m++)
 	{
 		concat[m] = s1[m];
